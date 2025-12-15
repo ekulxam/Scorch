56208d89a5347fb3c2133094a06ef182def30b1f
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-12-15 00:42:53 -0800

    Not sure how this is supposed to work

--- src/main/java/astrazoey/scorch/registry/ScorchAttachmentTypes.java
+++ src/main/java/astrazoey/scorch/registry/ScorchAttachmentTypes.java
@@ -5,7 +5,11 @@ import com.mojang.serialization.Codec;
 import net.fabricmc.fabric.api.attachment.v1.AttachmentRegistry;
 import net.fabricmc.fabric.api.attachment.v1.AttachmentSyncPredicate;
 import net.fabricmc.fabric.api.attachment.v1.AttachmentType;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricTrackedDataRegistry;
+import net.minecraft.entity.data.TrackedDataHandler;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.network.codec.PacketCodecs;
+import net.minecraft.util.Identifier;
 
 @SuppressWarnings("UnstableApiUsage")
 public class ScorchAttachmentTypes {
@@ -33,4 +37,11 @@ public class ScorchAttachmentTypes {
                     .persistent(Codec.INT)
                     .syncWith(PacketCodecs.VAR_INT, AttachmentSyncPredicate.all())
     );
+
+    public static TrackedDataHandler<Integer> HAIR_STYLE = registerFabricTrackedData(Scorch.id("hair_style"), TrackedDataHandlerRegistry.INTEGER);
+
+    public static <T> TrackedDataHandler<T> registerFabricTrackedData(Identifier id, TrackedDataHandler<T> trackedDataHandler) {
+        FabricTrackedDataRegistry.register(id, trackedDataHandler);
+        return (TrackedDataHandler<T>) FabricTrackedDataRegistry.get(id);
+    }
 }
